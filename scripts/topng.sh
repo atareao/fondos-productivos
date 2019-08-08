9192093292e000b8d7a33b24cd7d0706f80288f5
Miguel de Dios Matias <tres.14159@gmail.com>
2019-08-08 13:01:15 +0200

    Some changes and improves in the script topng.sh

--- scripts/topng.sh
+++ scripts/topng.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
+
+# Checks if the inkscape exists
+command -v inkscape >/dev/null 2>&1
+if [[ $? -ne 0 ]]
+then
+	echo >&2 "'inkscape' is required to export svg to png but it's not installed.  Aborting."
+	exit 1
+fi
+
 parentdir=$(dirname $(dirname $(readlink -f $0)))
 for i in $parentdir/src/*.svg
 do
 	src=$i
-    dest=$(basename $i)
+	dest=$(basename $i)
 	destfile=${dest/.svg/.png}
 	for j in $(ls -d $parentdir/fondos/*)
 	do
 		if [ "$j" != '.' ] && [ "$j" != '..' ]
 		then
-			res=$(basename $j)
+            res=$(basename $j)
             width=${res/x*/}
 			height=${res/*x/}
 			dest=$parentdir/fondos/$res/$destfile
+            echo ""
+            echo "Exporting $destfile (as ${width}x${height})"
+            echo "---------------------------------------"
 			inkscape $src --export-png=$dest --export-width=$width --export-height=$height
 		fi
 	done
